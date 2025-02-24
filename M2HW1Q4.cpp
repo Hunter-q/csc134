e4085047f1b8cdc79d21c8ceb2d5c9d184b09bbc
Hunter-q <55244594+Hunter-q@users.noreply.github.com>
2025-02-24 00:00:13 +0000

    M2HW1Q4

---
+++ M2HW1Q4.cpp
@@ -0,0 +1,29 @@
+//Hunter Pike
+//CSC134
+//2-23-25
+//M2HW1Q3
+
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Declare 
+    string school = "FTCC";
+    string team = "Trojans";
+    string letsGo = "Let's go ";
+    
+    // Cheers
+    string cheerOne = letsGo + school;
+    string cheerTwo = letsGo + team;
+
+    // Display
+    cout << cheerOne << endl;
+    cout << cheerOne << endl;
+    cout << cheerOne << endl;
+    cout << cheerTwo << endl;
+
+    return 0;
+}
